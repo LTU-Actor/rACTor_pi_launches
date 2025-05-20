14e2c5b2fc689a61aad24e077f924e68f7eb0bb9
rkaddis <rkaddis@ltu.edu>
2025-05-20 11:00:16 -0400

    add controls namespace

--- launch/controls.launch.py
+++ launch/controls.launch.py
@@ -2,26 +2,32 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, LogInfo, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, LogInfo, IncludeLaunchDescription, GroupAction
 from launch.substitutions import LaunchConfiguration, TextSubstitution
-from launch_ros.actions import Node, SetParameter
+from launch_ros.actions import Node, SetParameter, PushROSNamespace
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
+
+CONTROLS_NAMESPACE = "control"
 
 def mqtt_bridge():
     return Node(
         package='mqtt_wheel_bridge',
         executable='mqtt_wheel_bridge',
-        namespace="/",
+        namespace=CONTROLS_NAMESPACE,
         name='mqtt_wheel_bridge'
     )
 
 def control_tower():
-    return IncludeLaunchDescription(
+    tower = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('control_tower_ros2'), "control_tower.launch.py")
         )
     )
+    return GroupAction(actions=[
+        PushROSNamespace(CONTROLS_NAMESPACE),
+        tower
+    ])
 
 def generate_launch_description():
     
